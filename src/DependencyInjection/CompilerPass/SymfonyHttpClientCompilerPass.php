a6e660528609f494313d17b3953109cd69906079
Francis Hilaire <Prometee@users.noreply.github.com>
2021-09-16 19:16:18 +0200

    Add missing PSR-18 service

--- src/DependencyInjection/CompilerPass/SymfonyHttpClientCompilerPass.php
+++ src/DependencyInjection/CompilerPass/SymfonyHttpClientCompilerPass.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace FluxSE\SyliusHCaptchaPlugin\DependencyInjection\CompilerPass;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\HttpClient\Psr18Client;
 
 final class SymfonyHttpClientCompilerPass implements CompilerPassInterface
 {
@@ -20,6 +22,7 @@ final class SymfonyHttpClientCompilerPass implements CompilerPassInterface
         }
 
         $container->addDefinitions([
+            ClientInterface::class => new Definition(Psr18Client::class),
             RequestFactoryInterface::class => new Definition(Psr17Factory::class),
             StreamFactoryInterface::class => new Definition(Psr17Factory::class),
         ]);
