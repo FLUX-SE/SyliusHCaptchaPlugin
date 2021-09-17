2bc906833558298e5e1e31d00336c10196de83f9
Francis Hilaire <Prometee@users.noreply.github.com>
2021-09-17 19:22:20 +0200

    Remove bugfix

--- ecs.php
+++ ecs.php
@@ -9,9 +9,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/symfony.php');
 
     $services = $containerConfigurator->services();
-
-    /**
-     * Miss configured fixer into sylius-labs/coding-standard v4.1.0
-     */
-    $services->set(ClassAttributesSeparationFixer::class);
 };
