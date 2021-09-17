bdc3350cd476025240dc6c9d3e78b701c929d9e2
Francis Hilaire <Prometee@users.noreply.github.com>
2021-09-17 19:27:54 +0200

    Clean ecs file

--- ecs.php
+++ ecs.php
@@ -2,11 +2,8 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/symfony.php');
-
-    $services = $containerConfigurator->services();
 };
