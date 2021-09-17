8613d0dacdce9332a439c542733a2f91a984648e
Francis Hilaire <Prometee@users.noreply.github.com>
2021-09-17 19:28:17 +0200

    Readd missing service

--- src/FluxSESyliusHCaptchaPlugin.php
+++ src/FluxSESyliusHCaptchaPlugin.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace FluxSE\SyliusHCaptchaPlugin;
 
+use FluxSE\SyliusHCaptchaPlugin\DependencyInjection\CompilerPass\SymfonyHttpClientCompilerPass;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class FluxSESyliusHCaptchaPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container
+            ->addCompilerPass(new SymfonyHttpClientCompilerPass())
+        ;
+    }
 }
