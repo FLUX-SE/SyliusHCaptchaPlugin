dbb3a9181d898fdc2326363a91829855d2fa1919
Francis Hilaire <Prometee@users.noreply.github.com>
2024-12-20 00:15:45 +0100

    Refactor config and twig

--- src/DependencyInjection/FluxSESyliusHCaptchaExtension.php
+++ src/DependencyInjection/FluxSESyliusHCaptchaExtension.php
@@ -16,44 +16,4 @@ final class FluxSESyliusHCaptchaExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        if ($container->hasExtension('twig')) {
-            $container->prependExtensionConfig(
-                'twig',
-                [
-                    'form_themes' => [
-                        '@FluxSESyliusHCaptchaPlugin/hcaptcha.html.twig',
-                    ],
-                ],
-            );
-        }
-
-        if ($container->hasExtension('sylius_ui')) {
-            $container->prependExtensionConfig(
-                'sylius_ui',
-                [
-                    'events' => [
-                        'sylius.shop.contact.request.form' => [
-                            'blocks' => [
-                                'captcha' => [
-                                    'template' => '@FluxSESyliusHCaptchaPlugin/form.html.twig',
-                                    'priority' => -10,
-                                ],
-                            ],
-                        ],
-                        'sylius.shop.register.form' => [
-                            'blocks' => [
-                                'captcha' => [
-                                    'template' => '@FluxSESyliusHCaptchaPlugin/form.html.twig',
-                                    'priority' => -10,
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            );
-        }
-    }
 }
